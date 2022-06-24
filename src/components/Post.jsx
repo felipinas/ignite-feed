fccf1f24a8a8d7184be1ee12315448561fe3b5db
felipinas <jfss2@cin.ufpe.br>
2022-06-24 02:46:34 -0300

    fix: fix indentation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -19,25 +19,25 @@ export function Post(props) {
       </header>
 
       <div className={styles.content}>
-				<p>Fala galeraa 👋</p>
-				<p>
-					Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-				</p>
-				<p> 👉 <a href="">jane.design/doctorcare</a> </p>
-				<p> <a href="">#novoprojeto #nlw #rocketseat</a> </p>
+			  <p>Fala galeraa 👋</p>
+			  <p>
+			    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+			  </p>
+			  <p> 👉 <a href="">jane.design/doctorcare</a> </p>
+			  <p> <a href="">#novoprojeto #nlw #rocketseat</a> </p>
       </div>
 
-			<form className={styles.commentForm}>
-				<strong>Deixe seu feedback</strong>
+		  <form className={styles.commentForm}>
+			  <strong>Deixe seu feedback</strong>
 
-				<textarea
-					placeholder="Escreva um comentário..."
-				/>
+			  <textarea
+				  placeholder="Escreva um comentário..."
+			  />
         
         <footer>
           <button type="submit">Publicar</button>
         </footer>
-			</form>
+		  </form>
     </article>
   );
 }

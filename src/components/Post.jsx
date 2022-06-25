e0969c53804e81acb93bf7d06eb139389f3e18e8
felipinas <jfss2@cin.ufpe.br>
2022-06-25 00:47:28 -0300

    feat: add avatar and comments in post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(props) {
@@ -5,7 +7,7 @@ export function Post(props) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/felipinas.png" alt="" />
+          <Avatar src="https://github.com/felipinas.png" />
 
           <div className={styles.authorInfo}>
             <strong>João Felipe</strong>
@@ -38,6 +40,12 @@ export function Post(props) {
           <button type="submit">Publicar</button>
         </footer>
 		  </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }

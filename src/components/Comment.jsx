485d62861945b4fe3095bb8978ec8cac3e7e9831
felipinas <jfss2@cin.ufpe.br>
2022-07-21 00:25:04 -0300

    feat: make comment component dynamic

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { Avatar } from './Avatar';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
-export function Comment(props) {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/felipinas.png" />
@@ -23,7 +23,7 @@ export function Comment(props) {
             </button>
           </header>
 
-          <p>Muito bom, parabéns!!</p>
+          <p>{content}</p>
         </div>
 
         <footer>

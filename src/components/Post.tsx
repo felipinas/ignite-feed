d8aad2bf97b5e18fda45dac48392aaaa64524783
felipinas <jfss2@cin.ufpe.br>
2022-07-23 00:52:15 -0300

    fix: change content type to deploy

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -14,14 +14,14 @@ interface Author {
 
 interface Content {
   id: number;
-  type: 'paragraph' | 'link';
+  type: string;
   content: string;
 }
 
 interface PostProps {
   author: Author;
-  publishedAt: Date;
   content: Content[];
+  publishedAt: Date;
 }
 
 export function Post({ author, publishedAt, content }: PostProps) {
